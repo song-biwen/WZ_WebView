fe5ad37148cbeb6f994d34ec56091b5bd67d4472
songbiwen <1099790014@qq.com>
2016-11-18 11:20:44 +0800

    + 给网页中某个元素添加点击事件，js间接调用oc

---
+++ WZWebView/WZWebView/WZImageClickViewController.m
@@ -0,0 +1,39 @@
+//
+//  WZImageClickViewController.m
+//  WZWebView
+//
+//  Created by songbiwen on 2016/11/18.
+//  Copyright © 2016年 songbiwen. All rights reserved.
+//
+
+#import "WZImageClickViewController.h"
+
+@interface WZImageClickViewController ()
+
+@end
+
+@implementation WZImageClickViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.title = @"WZImageClickViewController";
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
--- WZWebView/WZWebView/WZWebViewController_01.m
+++ WZWebView/WZWebView/WZWebViewController_01.m
@@ -8,6 +8,7 @@
 
 #import "WZWebViewController_01.h"
 
+#define WZLink @"https://github.com/song-biwen/WZ_WebView"
 @interface WZWebViewController_01 ()
 <UIWebViewDelegate>
 @end
@@ -28,6 +29,27 @@
 #pragma mark -UIWebViewDelegate
 
 /**
+ 网页即将开始加载时，调用该方法
+
+ 可以监听网页里面的所有网络请求
+ 
+ ———— 实现js间接调用oc
+ @param webView <#webView description#>
+ @param request <#request description#>
+ @param navigationType <#navigationType description#>
+ @return yes 表示网络请求成功，no 表示阻止网络请求
+ */
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+    NSString *urlStr = request.URL.absoluteString;
+    if ([urlStr isEqualToString:WZLink]) {
+        Class class = NSClassFromString(@"WZImageClickViewController");
+        id subVC = [[class alloc] init];
+        [self.navigationController pushViewController:subVC animated:YES];
+        return NO;
+    }
+    return YES;
+}
+/**
  网页加载完成之后，调用该方法
 
  @param webView <#webView description#>
@@ -40,6 +62,9 @@
     [mutableStr appendString:@"var footer_btn = document.getElementsByClassName('footer-btn-fix')[0]; footer_btn.parentNode.removeChild(footer_btn);"];
     //删除版权
     [mutableStr appendString:@"var footer = document.getElementsByClassName('footer')[0]; footer.parentNode.removeChild(footer);"];
+    
+    //给图片添加点击事件
+    [mutableStr appendFormat:@"var imgTag = document.getElementsByTagName('figure')[0].children[0];imgTag.onclick = function imgTagClick() {window.location.href= '%@'};",WZLink];
     //注入js删除网页相应元素
     [webView stringByEvaluatingJavaScriptFromString:mutableStr];
 }
